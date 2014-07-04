01900713f41dc2aa474ad9fbbdafa5a23f3e1e5f
Trang Le <tragit93@gmail.com>
2014-07-04 14:04:09 +0700

    first commit

---
+++ beer.java
@@ -0,0 +1,21 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+
+public class beer {
+	public static void main (String[] args){
+		System.out.print("Lê Thị Trang \t");
+		System.out.print("11020345 \t");
+		System.out.print("K56CB \t");
+		System.out.print("Trangle93 \t");
+		System.out.println("tragit93@gmail.com \t\n");
+		
+		for(int i=9; i>1; i--){
+			System.out.println(i+ " bottles of beer on the wall,"+i+ " bottles of beer.");
+			System.out.println("Make Take one down, pass it around,");
+		}
+			System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+			System.out.println("Take one down, pass it around,");
+			System.out.println("No more bottles of beer on the wall.");
+	}
+
+}
